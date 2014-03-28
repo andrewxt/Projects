d86c0fba2b2aada46ebd268f25ecf62e50e937ea
andrewxt <andrewwchristie@gmail.com>
2014-03-28 13:38:21 -0400

    clean

---
+++ tagAssignment/tagAssignment/ACViewController.h
@@ -0,0 +1,16 @@
+//
+//  ACViewController.h
+//  tagAssignment
+//
+//  Created by Andrew Christie on 3/27/14.
+//  Copyright (c) 2014 Andrew Christie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *numberOfUsers;
+@property (strong, nonatomic) IBOutlet UILabel *numberOfTagsSent;
+@property (strong, nonatomic) IBOutlet UILabel *numberOfTagsReceived;
+@end
